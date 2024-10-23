d57d7440ba8a3e76f8baf82253d1477f5e21e548
cling088 <cling088@boomco.org>
2024-10-23 10:03:52 +0900

    [TEST] push test

--- src/pages/community/Detail.js
+++ src/pages/community/Detail.js
@@ -7,15 +7,21 @@ import {studyList, studyReplyList} from "../../data/study";
 import Badge from "../../components/common/Badge";
 import {COMMUNITY_TYPE, STATUS_DATA} from "../../util/const";
 import Reply from "../../components/common/Reply";
+import Axios from "../../api/api";
+import dayjs from "dayjs";
 
 const Detail = () => {
 
     const {user} = useUser();
     const {commId} = useParams();
-    const {state} = useLocation();
+    const location = useLocation();
+    const { stdyId } = location?.state || {};
 
     const navigate = useNavigate();
 
+    console.log("stdyId -> ", stdyId);
+
+    const [data, setData] = useState(null);
     const [studyData, setStudyData] = useState({});
     const [contentData, setContentData] = useState({});
     const [prevData, setPrevData] = useState(null);
@@ -62,93 +68,103 @@ const Detail = () => {
         });
     }
 
-    useEffect(() => {
-        console.log("replyData -> ", replyData)
-    }, [replyData])
+    const handleGetData = async () => {
+        await Axios.get(`/community/detail?stdyComtId=${commId}`, )
+            .then(function (response) {
+                setData(response.data);
+            })
+            .catch(function (error) {
+                console.log("error", error);
+            })
+    }
+
+    // useEffect(() => {
+    //     console.log("replyData -> ", replyData)
+    // }, [replyData])
 
     //@INFO 내용 세팅
     useEffect(() => {
         if(commId) {
-            const targetData = communityList.filter(item => item.id === parseInt(commId));
-            if(targetData?.length > 0) setContentData(targetData[0]);
+            handleGetData()
         }
     }, [commId]);
 
     //@INFO reply 내용 가져요기
-    useEffect(() => {
-        if(commReplyList && commId) {
-            const replyData = commReplyList.filter(item => item.id === parseInt(commId));
-            if(replyData?.length > 0) {
-                const newData = replyData[0]?.replyList?.map(item => ({
-                    ...item,
-                    isEdit: false, // 수정모드
-                    isReply: false, // 대댓글상태
-                    reReplyList: item?.reReplyList?.map(re => ({
-                        ...re,
-                        isEdit: false // 대댓글 수정모드
-                    }))
-                }))
-                setReplyData({
-                    ...replyData[0],
-                    replyList: newData
-                });
-            } else {
-                setReplyData([]);
-            }
-        }
-    }, [commReplyList, commId]);
+    // useEffect(() => {
+    //     if(commReplyList && commId) {
+    //         const replyData = commReplyList.filter(item => item.id === parseInt(commId));
+    //         if(replyData?.length > 0) {
+    //             const newData = replyData[0]?.replyList?.map(item => ({
+    //                 ...item,
+    //                 isEdit: false, // 수정모드
+    //                 isReply: false, // 대댓글상태
+    //                 reReplyList: item?.reReplyList?.map(re => ({
+    //                     ...re,
+    //                     isEdit: false // 대댓글 수정모드
+    //                 }))
+    //             }))
+    //             setReplyData({
+    //                 ...replyData[0],
+    //                 replyList: newData
+    //             });
+    //         } else {
+    //             setReplyData([]);
+    //         }
+    //     }
+    // }, [commReplyList, commId]);
 
     // @INFO 이전, 다음 컨텐츠 찾기
-    useEffect(() => {
-        if(communityList && commId) {
-            const findPrevNextData = (data, targetId) => {
-                let prevData = null;
-                let nextData = null;
-                const targetIndex = data.findIndex(item => item.id === parseInt(targetId));
-                if (targetIndex !== -1) {
-                    if (targetIndex > 0) {
-                        prevData = data[targetIndex - 1];
-                    }
-                    if (targetIndex < data.length - 1) {
-                        nextData = data[targetIndex + 1];
-                    }
-                }
-                setPrevData(prevData);
-                setNextData(nextData);
-            };
+    // useEffect(() => {
+    //     if(communityList && commId) {
+    //         const findPrevNextData = (data, targetId) => {
+    //             let prevData = null;
+    //             let nextData = null;
+    //             const targetIndex = data.findIndex(item => item.id === parseInt(targetId));
+    //             if (targetIndex !== -1) {
+    //                 if (targetIndex > 0) {
+    //                     prevData = data[targetIndex - 1];
+    //                 }
+    //                 if (targetIndex < data.length - 1) {
+    //                     nextData = data[targetIndex + 1];
+    //                 }
+    //             }
+    //             setPrevData(prevData);
+    //             setNextData(nextData);
+    //         };
+    //
+    //         findPrevNextData(communityList, commId);
+    //     }
+    // }, [communityList, commId])
 
-            findPrevNextData(communityList, commId);
-        }
-    }, [communityList, commId])
+    // useEffect(() => {
+    //     if(state?.id) {
+    //         setStudyData(state);
+    //     }
+    // }, [state])
 
-    useEffect(() => {
-        if(state?.id) {
-            setStudyData(state);
-        }
-    }, [state])
 
     return (
         <Layout>
             {
-                contentData &&
+                data &&
                 <div className={"page_wrap comm_detail_page detail_page default_width"}>
                     {/*content_section start*/}
                     <section className={"content_section"}>
                         <div className="content_header_wrap">
                             <h3 className={"title"}>
-                                <b>[{COMMUNITY_TYPE[`${contentData.postType}`]}]</b>
-                                { contentData.title }
+                                <b>[{COMMUNITY_TYPE[`${data.stdyComSt}`]}]</b>
+                                { data.stdyComTitle }
                             </h3>
                         </div>
                         <div className="info_wrap">
                             <div className="left_section">
-                                <p className="writer">{contentData?.writer?.userId}</p>
+                                <p className="writer">{data?.amnnUserId}</p>
                                 <div className="sub_info_wrap">
-                                    <span>작성일: {contentData.createDate}</span>
+                                    <span>작성일: {data?.amnnTs && dayjs(data.amnnTs).format("YYYY-MM-DD HH:mm")}</span>
                                 </div>
                             </div>
                             {
-                                (user.userIdx === contentData?.writer?.userIdx) &&
+                                (user.userId === data?.rgsnUserId) &&
                                 <div className="right_section">
                                     <button className="button linear" onClick={() => navigate(`/community/write/${commId}`)}>수정</button>
                                     <button className="button">삭제</button>
@@ -156,11 +172,11 @@ const Detail = () => {
                             }
                         </div>
                         <div className="content_wrap">
-                            { contentData.content }
+                            { data?.stdyComCon }
                         </div>
                         <div className="button_group">
                             <button
-                                onClick={() => navigate(`/community/detail/${prevData.id}`, { state: state })}
+                                onClick={() => navigate(`/community/detail/${prevData.id}`, {state: {stdyId: stdyId}})}
                                 className={"button linear"}
                                 disabled={!prevData}
                             >
@@ -168,16 +184,16 @@ const Detail = () => {
                             </button>
                             <button
                                 className={"button linear"}
-                                onClick={() => navigate(`/community/${state.id}`)}
+                                onClick={() => navigate(`/community/${stdyId}`)}
                             >
                                 목록
                             </button>
                             <button
                                 className={"button linear"}
-                                onClick={() => navigate(`/community/detail/${nextData.id}`, { state: state })}
+                                onClick={() => navigate(`/community/detail/${nextData.id}`, {state: {stdyId: stdyId}})}
                                 disabled={!nextData}
                             >
-                                다음글&nbsp;&nbsp;→
+                                다음글&nbsp;&nbsp; →
                             </button>
                         </div>
                         {
@@ -194,12 +210,16 @@ const Detail = () => {
                     {/*participant_section start*/}
                     <section className="participant_section">
                         <div className="participants_list_wrap">
-                            <p className="label">참여인원 {studyData?.participants?.length ?? 0}명</p>
+                            <p className="label">참여인원 {(data?.memberList?.length - 1) ?? 0}명</p>
                             <div className="participants_list">
                                 {
-                                    studyData?.participants?.map((item, idx) => (
-                                        <span className={"participant"}>{item.userName}</span>
+                                    data?.memberList?.filter(member => member.userId !== user.userId)?.map((item, idx) => (
+                                        <span className={"participant"}>{item.userNm}</span>
                                     ))
+                                }
+                                {
+                                    (data?.memberList?.filter(member => member.userId !== user.userId)?.length <= 0) &&
+                                    <p>신청한 회원이 없습니다.</p>
                                 }
                             </div>
                         </div>
